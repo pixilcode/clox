b4280fef5554e44fb6685eaed0a3aa42678c31e5
Brendon Bown <bhb49@byu.edu>
2022-12-12 15:49:36 -0700

    Initialize a chunk

--- src/main.c
+++ src/main.c
@@ -2,5 +2,9 @@
 #include "chunk.h"
 
 int main(int argc, const char* argv[]) {
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    freeChunk(&chunk);
     return 0;
 }
